d0cbdb3fc4b14cc5b71fcfad76a184c097760437
dnhmd <adnnahmed@gmail.com>
2026-06-22 14:45:46 +0300

    bootstrap project with Spring Boot, Postgres, Redis, Flyway

---
+++ backend/src/test/java/com/shorthand/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shorthand.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
