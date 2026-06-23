9582406bf67915107d4fe8d39c34f2fe16e2749e
dnhmd <adnnahmed@gmail.com>
2026-06-23 15:42:28 +0300

    feat: add RedirectLinkService with cache-aside pattern

---
+++ backend/src/main/java/com/shorthand/backend/application/service/RedirectLinkService.java
@@ -0,0 +1,52 @@
+package com.shorthand.backend.application.service;
+
+import com.shorthand.backend.domain.model.Link;
+import com.shorthand.backend.domain.port.inbound.RedirectLinkUseCase;
+import com.shorthand.backend.domain.port.outbound.LinkCachePort;
+import com.shorthand.backend.domain.port.outbound.LinkRepository;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
+
+public class RedirectLinkService implements RedirectLinkUseCase {
+
+    private final LinkCachePort linkCachePort;
+    private final LinkRepository linkRepository;
+
+    public RedirectLinkService(LinkCachePort linkCachePort, LinkRepository linkRepository) {
+        this.linkCachePort = linkCachePort;
+        this.linkRepository = linkRepository;
+    }
+
+    @Override
+    public String redirect(String code) {
+        Optional<Link> cachedLink = linkCachePort.get(code);
+        if (cachedLink.isPresent()) {
+            Link link = cachedLink.get();
+            checkExpiry(code, link);
+
+            return link.originalUrl();
+        } else {
+            // @TODO - Log Cache Miss
+        }
+
+        Optional<Link> dbLink = linkRepository.findByCode(code);
+        if (dbLink.isPresent()) {
+            Link link = dbLink.get();
+            checkExpiry(code, link);
+            Duration ttl = Duration.between(Instant.now(), link.expiresAt());
+            linkCachePort.put(code, link, ttl);
+
+            return link.originalUrl();
+        }
+
+        throw new RuntimeException("Link Unavailable");
+    }
+
+    private void checkExpiry(String code, Link link) {
+        if (link.isExpired()) {
+            throw new RuntimeException("Link Expired");
+        }
+    }
+}
